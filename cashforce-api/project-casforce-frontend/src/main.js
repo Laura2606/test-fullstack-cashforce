ad04d9d2dfea5ecc07580754f151dd8ec8a2deaa
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-08-02 15:35:42 -0300

    Instala vue e cria arquivos para o frontend

--- cashforce-api/project-casforce-frontend/src/main.js
+++ cashforce-api/project-casforce-frontend/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import Home from './views/HomeView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+    { path: '/', component: Home },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
---
+++ cashforce-api/project-casforce-frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <OrderList />
+  </div>
+</template>
+
+<script>
+import OrderList from '../components/OrderList.vue';
+
+export default {
+  components: {
+    OrderList,
+  },
+};
+</script>
